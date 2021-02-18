90d4c2a9fb6d7912d28a7bccfb52bc48d736f460
lucasfe4nor <lukasfeanorzr5123@hotmail.com>
2021-02-17 23:03:30 -0300

    Update findById

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -63,11 +63,43 @@ export default {
 
   async modifyProduct(req, res) {
     const { id } = req.params;
+    const {
+      name,
+      price,
+      perishable,
+      manufactureDate,
+      expirationDate,
+    } = req.body;
 
     try {
-      const product = await Product.findByIdAndUpdate(id, req.body, {
-        new: true,
-      });
+      if (!name || !price || !manufactureDate || perishable === undefined) {
+        throw new Error('Campos devem ser preenchidos');
+      }
+
+      if (expirationDate && manufactureDate) {
+        const expiration = new Date(expirationDate);
+        const manufacture = new Date(manufactureDate);
+
+        if (manufacture.getTime() > expiration.getTime()) {
+          throw new Error(
+            'Data de fabricação não deve ser maior que data de validade'
+          );
+        }
+      }
+
+      const product = await Product.findByIdAndUpdate(
+        id,
+        {
+          name,
+          price,
+          perishable,
+          manufacture_date: manufactureDate,
+          expiration_date: expirationDate,
+        },
+        {
+          new: true,
+        }
+      );
 
       res.status(200).send({ product });
     } catch (error) {
